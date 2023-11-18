3499b48e8a53fa426ea363a74486d3d28d7d56a2
BENJA <benjamin.soto1@virginiogomez.cl>
2023-11-18 17:47:09 -0300

    ACTUALIZACION 18.11 17:50

--- app/src/main/java/cl/example/evaluacion/Clases/Mymaps.java
+++ app/src/main/java/cl/example/evaluacion/Clases/Mymaps.java
@@ -9,29 +9,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import cl.example.evaluacion.Inicio;
 import cl.example.evaluacion.R;
 
-public class Mymaps extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
+public class Mymaps extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener, GoogleMap.OnMarkerClickListener {
 
     EditText txtLatitud, txtLongitud;
     Button btMenu;
     GoogleMap mMap;
+
+    // Lista para almacenar los marcadores
+    List<Marker> marcadores = new ArrayList<>();
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mymaps);
 
-        btMenu=findViewById(R.id.btMenu);
+        btMenu = findViewById(R.id.btMenu);
         txtLatitud = findViewById(R.id.txtLatitud);
         txtLongitud = findViewById(R.id.txtLongitud);
         Intent intent = new Intent(this, Inicio.class);
@@ -53,30 +61,84 @@ public class Mymaps extends AppCompatActivity implements OnMapReadyCallback, Goo
         this.mMap.setOnMapClickListener(this);
         this.mMap.setOnMapLongClickListener(this);
 
-        LatLng tienda = new LatLng(-36.5900806,-72.0821342);
-        mMap.addMarker(new MarkerOptions().position(tienda).title("Tienda"));
+        // Marcador para la tienda
+        LatLng tienda = new LatLng(-36.5900806, -72.0821342);
+        Marker tiendaMarker = mMap.addMarker(new MarkerOptions().position(tienda).title("Tienda"));
+        tiendaMarker.setTag("Información de la tienda");
+
+        // Otros marcadores
+        LatLng marker1 = new LatLng(-36.6064595, -72.1054738);
+        Marker marker1Marker = mMap.addMarker(new MarkerOptions().position(marker1).title("broderjud").snippet("BARBERIA ASDFJKMASFMSAKFMSKAMFKDSM"));
+        marker1Marker.setTag("Información de broderjud");
+
+        LatLng marker2 = new LatLng(-36.6057102, -72.0944875);
+        Marker marker2Marker = mMap.addMarker(new MarkerOptions().position(marker2).title("barber desenden").snippet("CORTES DEGRADESSSSSSS"));
+        marker2Marker.setTag("Información de barber desenden");
+
+        // Agrega los marcadores a la lista
+        marcadores.add(tiendaMarker);
+        marcadores.add(marker1Marker);
+        marcadores.add(marker2Marker);
+
+        // Mueve la cámara al primer marcador
         mMap.moveCamera(CameraUpdateFactory.newLatLng(tienda));
+
+        // Configura el listener para los clics en marcadores
+        mMap.setOnMarkerClickListener(this);
+
+        // Configura el adaptador para el InfoWindow
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null; // Usa la ventana de información predeterminada
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                // Personaliza el contenido del InfoWindow
+                View infoWindow = getLayoutInflater().inflate(R.layout.custom_info_window, null);
+
+                // Obtén la información almacenada en setTag
+                Object tag = marker.getTag();
+
+                // Muestra la información en el InfoWindow personalizado
+                if (tag != null) {
+                    TextView infoTextView = infoWindow.findViewById(R.id.infoTextView);
+                    infoTextView.setText(tag.toString());
+                }
+
+                return infoWindow;
+            }
+        });
     }
 
     @Override
     public void onMapClick(@NonNull LatLng latLng) {
         txtLatitud.setText(""+latLng.latitude);
         txtLongitud.setText(""+ latLng.longitude);
-
-        mMap.clear();
-        LatLng tienda = new LatLng(latLng.latitude,latLng.longitude);
-        mMap.addMarker(new MarkerOptions().position(tienda).title(""));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(tienda));
     }
 
     @Override
     public void onMapLongClick(@NonNull LatLng latLng) {
         txtLatitud.setText(""+latLng.latitude);
         txtLongitud.setText(""+ latLng.longitude);
-
-        mMap.clear();
-        LatLng tienda = new LatLng(latLng.latitude,latLng.longitude);
-        mMap.addMarker(new MarkerOptions().position(tienda).title(""));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(tienda));
     }
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        // Muestra la información del marcador al hacer clic
+        marker.showInfoWindow();
+
+        // Obtiene las coordenadas del marcador
+        LatLng markerPosition = marker.getPosition();
+        double latitud = markerPosition.latitude;
+        double longitud = markerPosition.longitude;
+
+        // Muestra las coordenadas en los campos de texto
+        txtLatitud.setText(String.valueOf(latitud));
+        txtLongitud.setText(String.valueOf(longitud));
+
+        return true;
+    }
+
+
 }
